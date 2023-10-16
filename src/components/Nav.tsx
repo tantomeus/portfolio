caec4d9d059257fdb684b2c42b8a8940e8b2add4
tantomeus <carlomanid@gmail.com>
2023-10-16 18:41:26 +0300

    Add theme change functionality

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,18 @@
-import { FaMoon, FaGlobe } from "react-icons/fa";
+"use client";
+
+import { FaMoon, FaSun, FaGlobe } from "react-icons/fa";
 
 export default function Nav() {
+    
+    function handleToggleTheme(e: MouseEvent): void {
+        const html = document.querySelector("html")!;
+        if (html.className.includes("dark")) {
+            html.className = "light";
+        } else {
+            html.className = "dark";
+        }
+    }
+
     return (
     <nav className="flex items-center px-6 lg:px-10 py-4 top-0 bg-white/80
     dark:bg-gray-900/80 backdrop-blur-md shadow-md fixed w-full z-50">
@@ -14,8 +26,11 @@ export default function Nav() {
         </ul>
 
         <div className="ml-9 flex gap-4">
-            <button className="nav-button">{<FaMoon/>} Dark</button>
-            <button className="nav-button">{<FaGlobe/>} Eng</button>
+            <button onClick={handleToggleTheme} aria-label="toggle theme">
+                <span className="hidden dark:flex nav-button">{<FaMoon/>} Dark</span>
+                <span className="flex dark:hidden nav-button">{<FaSun/>} Light</span>
+                </button>
+            <button aria-label="toggle language" className="nav-button">{<FaGlobe/>} Eng</button>
         </div>
     </nav>
     )
