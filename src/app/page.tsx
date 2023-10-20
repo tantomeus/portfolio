3038aefdc716900caeecb750076d5767ea35776e
tantomeus <carlomanid@gmail.com>
2023-10-20 22:59:26 +0300

    Include sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import About from '@/components/About';
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="min-h-screen">
+      <Hero/>
+      <About/>
+      <Projects/>
     </main>
   )
 }
