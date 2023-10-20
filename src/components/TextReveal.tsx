076acbb1b91ea1fc80b7ac748dda1236a212bb81
tantomeus <carlomanid@gmail.com>
2023-10-20 23:00:43 +0300

    Remove unnecessary function

--- src/components/TextReveal.tsx
+++ src/components/TextReveal.tsx
@@ -1,22 +1,18 @@
 export default function TextReveal({ delay, colors, className="", children }: 
-{delay: number, colors: string, className?: string, children: JSX.Element}) {
-
-    function makeColorClass(targ: string) {
-        return colors.split(" ").map((color) => color.includes("dark") ? `${color.slice(0, 5)}${targ}-${color.slice(5)}` : `${targ}-${color}`).join(" ");
-    }
+{delay: number, colors: {bg: string, text: string}, className?: string, children: JSX.Element}) {
 
     return (
-    <div className={`relative overflow-x-clip ${className}`}>
+    <div className={`relative overflow-x-clip pl-[1px] ${className}`}>
 
-        <div data-aos="zoom-in" data-aos-delay={delay + 300} data-aos-duration="500" 
-        className={`inline ${makeColorClass("text")}`}>
+        <div data-aos="fade" data-aos-delay={delay + 300} data-aos-duration="500" 
+        className={`inline ${colors.text}`}>
             {children}
         </div>
 
         <div data-aos="slide-right" data-aos-delay={delay} data-aos-duration="1000"
         className="flex w-[200%] absolute -inset-y-1 pointer-events-none">
-            <div className="h-full w-1/2"></div>
-            <div className={`${makeColorClass("bg")} h-full grow mx-4`}></div>
+            <div className="w-1/2 h-full"></div>
+            <div className={`${colors.bg} h-full grow mx-4`}></div>
         </div>
     </div>
     )
