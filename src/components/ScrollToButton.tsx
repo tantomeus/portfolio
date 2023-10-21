2477d4c64da136406d2e903130b7f1ee694ed953
tantomeus <carlomanid@gmail.com>
2023-10-21 22:15:58 +0300

    Fix colors

--- src/components/ScrollToButton.tsx
+++ src/components/ScrollToButton.tsx
@@ -11,7 +11,7 @@ export default function ScrollToButton() {
     return (
     <button onClick={handleScroll} aria-label="go to about me"
     data-aos="fade-down" data-aos-delay="1500" data-aos-duration="700" data-aos-offset="-200"
-    className="flex justify-center py-5 text-gray-700 dark:text-white absolute w-14 mx-auto bottom-10 right-0 left-0">
+    className="flex justify-center py-5 text-slate-700 dark:text-slate-100 absolute w-14 mx-auto bottom-10 right-0 left-0">
         <TbChevronDown className="pulse text-3xl sm:text-4xl md:text-5xl"/>
     </button>
     )
