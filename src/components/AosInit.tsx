faae1f5c2a6c628520e2737afc181b054c3c1963
tantomeus <carlomanid@gmail.com>
2023-10-26 23:25:44 +0300

    Set offset to 0

--- src/components/AosInit.tsx
+++ src/components/AosInit.tsx
@@ -1,16 +1,16 @@
 "use client"
 
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default function AOSInit () {
   useEffect(() => {
     AOS.init({
-      easing: 'ease-out-quad',
-      duration: 1000,
+      offset: 0,
     });
-  }, [])
+  }, []);
 
   return null
 }
