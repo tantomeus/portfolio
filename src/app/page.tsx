248a0d6025cc05a9e4aeabd0c3b8c194f37e5120
tantomeus <carlomanid@gmail.com>
2023-10-29 23:43:45 +0300

    Include contact and footer sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import About from '@/components/About';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 
@@ -8,6 +10,8 @@ export default function Home() {
       <Hero/>
       <About/>
       <Projects/>
+      <Contact/>
+      <Footer/>
     </main>
   )
 }
