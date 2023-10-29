dae177a714ea7babcf26effd3bd2b926c1098397
tantomeus <carlomanid@gmail.com>
2023-10-29 23:44:19 +0300

    Add states and subtim button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,27 +4,33 @@ import { useState } from "react";
 
 export default function Form() {
     const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
     return (
         <form
         // data-aos="flip-up" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="-200"
-        className="space-y-5 text-slate-800 grow ">
+        className="space-y-5 text-slate-800 grow text-right">
             <div className="relative">
                 <input value={username} onChange={(e) => setUsername(e.target.value)}
                 className="w-full h-10 px-4 py-2 bg-transparent border-2 outline-none peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"/>
-                <label className={`absolute top-5 -translate-y-1/2 left-3 transition duration-200 peer-focus:scale-[80%] origin-top-left bg-slate-900 px-1 text-slate-600 peer-hover:text-slate-300  peer-focus:text-slate-50 peer-focus:translate-y-[-125%] pointer-events-none ${username ? "translate-y-[-125%] scale-[80%] text-stone-50" : ""}`}>Your name</label>
+                <label className={`input ${username ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your name</label>
             </div>
             
             <div className="relative">
-                <input value={username} onChange={(e) => setUsername(e.target.value)}
+                <input value={email} onChange={(e) => setEmail(e.target.value)}
                 className="w-full h-10 px-4 py-2 bg-transparent border-2 outline-none peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"/>
-                <label className={`absolute top-5 -translate-y-1/2 left-3 transition duration-200 peer-focus:scale-[80%] origin-top-left bg-slate-900 px-1 text-slate-600 peer-hover:text-slate-300  peer-focus:text-slate-50 peer-focus:translate-y-[-125%] pointer-events-none ${username ? "translate-y-[-125%] scale-[80%] text-stone-50" : ""}`}>Your email</label>
+                <label className={`input ${email ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your email</label>
             </div>
 
             <div className="relative">
-                <textarea name="" id="" cols={10} className="w-full px-4 py-2 bg-transparent border-2 outline-none min-h-1/4 peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"></textarea>
-                <label className={`absolute top-5 -translate-y-1/2 left-3 transition duration-200 peer-focus:scale-[80%] origin-top-left bg-slate-900 px-1 text-slate-600 peer-hover:text-slate-300  peer-focus:text-slate-50 peer-focus:translate-y-[-125%] pointer-events-none ${username ? "translate-y-[-125%] scale-[80%] text-stone-50" : ""}`}>Your message</label>
+                <textarea name="" id="" cols={10} value={message} onChange={(e) => setMessage(e.target.value)}
+                className="w-full px-4 py-2 bg-transparent border-2 outline-none min-h-1/4 peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"></textarea>
+                <label className={`input ${message ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your message</label>
             </div>
+
+            <button type="submit"
+            className="px-2 py-1 text-base font-semibold transition-colors bg-blue-200 border border-blue-400 rounded-sm shadow-lg xs:px-4 shadow-blue-300/20 hover:bg-blue-300 roboto-mono">Send</button>
         </form>
     )
 }
