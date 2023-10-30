e4b245e45bf55d5d7f1c0e5baa487a267a063a38
tantomeus <carlomanid@gmail.com>
2023-10-30 17:30:31 +0300

    Add an interface for projects

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,17 @@
 export interface Tool {
     name: string,
-    descr: string
+    descr: string,
+    color: string,
+    experience: string,
+    link: string
 };
+
+export interface Project {
+    name: string,
+    image: string,
+    description: string,
+    tools: string[],
+    link: string,
+    github: string,
+    year: number
+}
