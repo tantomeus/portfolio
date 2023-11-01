10756cab5eab38b408c3e5b92ca31a8036602122
tantomeus <carlomanid@gmail.com>
2023-11-01 22:38:10 +0300

    Fix classname

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -13,20 +13,20 @@ export default function Form() {
         className="space-y-5 text-slate-800 grow text-right">
             <div className="relative">
                 <input value={username} onChange={(e) => setUsername(e.target.value)}
-                className="w-full h-10 px-4 py-2 bg-transparent border-2 outline-none peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"/>
-                <label className={`input ${username ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your name</label>
+                className="w-full h-10 px-4 py-2 bg-transparent border-2 outline-none peer border-slate-600 text-slate-900 focus:border-slate-900 dark:focus:border-slate-50 dark:text-slate-50"/>
+                <label className={`label ${username ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your name</label>
             </div>
             
             <div className="relative">
                 <input value={email} onChange={(e) => setEmail(e.target.value)}
-                className="w-full h-10 px-4 py-2 bg-transparent border-2 outline-none peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"/>
-                <label className={`input ${email ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your email</label>
+                className="w-full h-10 px-4 py-2 bg-transparent border-2 outline-none peer border-slate-600 text-slate-900 focus:border-slate-900 dark:focus:border-slate-50 dark:text-slate-50"/>
+                <label className={`label ${email ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your email</label>
             </div>
 
             <div className="relative">
                 <textarea name="" id="" cols={10} value={message} onChange={(e) => setMessage(e.target.value)}
-                className="w-full px-4 py-2 bg-transparent border-2 outline-none min-h-1/4 peer border-slate-600 hover:border-slate-600 focus:border-slate-50 text-slate-50"></textarea>
-                <label className={`input ${message ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your message</label>
+                className="w-full px-4 py-2 bg-transparent border-2 outline-none min-h-1/4 peer border-slate-600 text-slate-900 focus:border-slate-900 dark:focus:border-slate-50 dark:text-slate-50"></textarea>
+                <label className={`label ${message ? "translate-y-[-125%] scale-[80%]" : ""}`}>Your message</label>
             </div>
 
             <button type="submit"
