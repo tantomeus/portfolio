ebdea7e6ff88095f2b037e080d03fc0be1146bee
tantomeus <carlomanid@gmail.com>
2023-11-01 22:35:30 +0300

    Add github

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import { AiFillGithub } from "react-icons/ai";
+
 export default function Footer() {
     return (
-    <footer>
-        
+    <footer className="from-slate-200 to-slate-50 bg-gradient-to-b dark:to-slate-800 dark:from-slate-900 py-8 text-center">
+        <div className="flex justify-center">
+            <a href="#" aria-label="link to my github account"><AiFillGithub className="h-12 w-12"/></a>
+        </div>
     </footer>
     )
 }
