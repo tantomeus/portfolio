b71b08c6005748422f0d92429cd6f1d980293512
tantomeus <carlomanid@gmail.com>
2023-11-02 23:33:36 +0300

    Add bottom condition

--- src/components/ScrollToButton.tsx
+++ src/components/ScrollToButton.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import { TbChevronDown } from "react-icons/tb";
+import { TbChevronDown, TbChevronUp } from "react-icons/tb";
 
-export default function ScrollToButton() {
+export default function ScrollToButton({ position = "top" }) {
+    const isBottom = position === "bottom";
 
     function handleScroll() {
-        document.querySelector("#about")?.scrollIntoView({behavior: "smooth"});
+        if (isBottom) document.querySelector("#hero")?.scrollIntoView({behavior: "smooth"});
+        else document.querySelector("#about")?.scrollIntoView({behavior: "smooth"});
     }
 
     return (
-    <button onClick={handleScroll} aria-label="go to about me"
-    data-aos="fade-down" data-aos-delay="1500" data-aos-duration="700" data-aos-offset="-200"
+    <button onClick={handleScroll} aria-label="scroll to page"
+    data-aos="fade-down" data-aos-delay={isBottom ? "500" : "1500"} data-aos-duration="700" data-aos-offset="-200"
     className="flex justify-center py-5 text-slate-700 dark:text-slate-100 absolute w-14 mx-auto bottom-10 right-0 left-0">
-        <TbChevronDown className="pulse text-3xl sm:text-4xl md:text-5xl"/>
+        {isBottom && <TbChevronUp className="pulse text-3xl sm:text-4xl md:text-5xl"/>}
+        {!isBottom && <TbChevronDown className="pulse text-3xl sm:text-4xl md:text-5xl"/>}
     </button>
     )
 }
